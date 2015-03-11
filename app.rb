c117ffeaa2b54af2f5eb0d793ed58cf626d13dd4
chenlizhan <chung1350@hotmail.com>
2015-03-11 19:53:05 +0800

    Moved the save machnism to back-end

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra/base'
 
 class PageMagicApp < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
   get '/' do
     erb :home
+  end
+
+  post '/api/v1/save' do
+    content = request.body.read
+    logger.info content
+    
+    File.open('draft/temp.txt', 'w') do |file|
+      file.puts content
+    end
   end
 end
