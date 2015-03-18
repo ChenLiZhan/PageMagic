14b3de0f3e475d760f2dbd32f2afc46f10e2cc1b
chenlizhan <chung1350@hotmail.com>
2015-03-18 21:00:15 +0800

    Saving status feature
    + Use javascript to implement the saving status check
    + Create a new api to compare the hash of the new and original file
    content

--- app.rb
+++ app.rb
@@ -1,23 +1,36 @@
 require 'sinatra/base'
+require 'digest/sha1'
 
 class PageMagicApp < Sinatra::Base
   configure :production, :development do
     enable :logging
   end
+
   get '/' do
-    @content
     File.open('public/draft/temp.txt', 'r') do |file|
       @content = file.read
     end
+    logger.info @content
     erb :home
   end
 
   post '/api/v1/save' do
     content = request.body.read
-    logger.info content
-    
+
     File.open('public/draft/temp.txt', 'w') do |file|
       file.puts content
+    end
+  end
+
+  post '/api/v1/compare' do
+    File.open('public/draft/temp.txt', 'r') do |file|
+      origin_content = Digest::SHA1.hexdigest file.read
+      current_content = Digest::SHA1.hexdigest request.body.read
+      if origin_content === current_content
+        return 'not change'
+      else
+        return 'change'
+      end
     end
   end
 end
